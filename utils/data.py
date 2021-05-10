c6c57fb7deaab0470f0f3cf3cfeb559341459f3e
Wang Zejun <zejunwang@pku.edu.cn>
2021-05-10 13:39:03 +0800

    bug fixed for dataset

--- utils/data.py
+++ utils/data.py
@@ -7,7 +7,7 @@ from dgl.data.utils import load_graphs
 
 class ASTDataset(DGLDataset):
     def __getitem__(self, idx):
-        return self.graphs[idx], self.label_dict["labels"][idx]
+        return self.graphs[idx], self.label_dict["label"][idx] - 1  # because the labels start from 1
 
     def __len__(self):
         return len(self.graphs)
@@ -16,7 +16,7 @@ class ASTDataset(DGLDataset):
         pass
 
     def __init__(self, data_type, raw_dir=None, save_dir=None):
-        super(DGLDataset, self).__init__(raw_dir=raw_dir, save_dir=save_dir)
+        super(ASTDataset, self).__init__(name="ASTDataset", raw_dir=raw_dir, save_dir=save_dir)
         self.data_type = data_type
         self.graphs = list([])  # space for readable
         self.label_dict = {"labels": list([])}  # space for readable
@@ -37,4 +37,4 @@ class ASTDataset(DGLDataset):
     def load(self):
         data_path = os.path.join(self.save_dir, self.data_type+".bin")
         self.graphs, self.label_dict = load_graphs(data_path)
-        self.n_labels = torch.max(self.label_dict["labels"].item())
+        self.n_labels = torch.max(self.label_dict["label"]).item()
